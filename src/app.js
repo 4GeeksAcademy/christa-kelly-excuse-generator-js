346510a774b704d48c507a508379044a1b25ffbc
Christa Kelly <46685557+christadkelly@users.noreply.github.com>
2023-08-24 15:22:01 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
+import "./index.html";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed on", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoNum = Math.floor(Math.random() * who.length);
+  let actionNum = Math.floor(Math.random() * action.length);
+  let whatNum = Math.floor(Math.random() * what.length);
+  let whenNum = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoNum] +
+    " " +
+    action[actionNum] +
+    " " +
+    what[whatNum] +
+    " " +
+    when[whenNum]
+  );
 };
