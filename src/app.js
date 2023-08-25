22cc1dd559f85f0b12a8fa90682e0a538decd570
Christa Kelly <46685557+christadkelly@users.noreply.github.com>
2023-08-25 16:13:50 +0000

    added images

--- src/app.js
+++ src/app.js
@@ -3,35 +3,43 @@ import "bootstrap";
 import "./index.html";
 
 window.onload = () => {
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#excuse").innerHTML = generateExcuse();
-  });
-};
+  function excuse() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed on", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
 
-let generateExcuse = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed on", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+    let whoNum = Math.floor(Math.random() * who.length);
+    let actionNum = Math.floor(Math.random() * action.length);
+    let whatNum = Math.floor(Math.random() * what.length);
+    let whenNum = Math.floor(Math.random() * when.length);
 
-  let whoNum = Math.floor(Math.random() * who.length);
-  let actionNum = Math.floor(Math.random() * action.length);
-  let whatNum = Math.floor(Math.random() * what.length);
-  let whenNum = Math.floor(Math.random() * when.length);
+    let excuseArray = [
+      who[whoNum] +
+        " " +
+        action[actionNum] +
+        " " +
+        what[whatNum] +
+        " " +
+        when[whenNum]
+    ];
 
-  return (
-    who[whoNum] +
-    " " +
-    action[actionNum] +
-    " " +
-    what[whatNum] +
-    " " +
-    when[whenNum]
-  );
+    let img = document.getElementById("image");
+    let pics = [
+      "https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J",
+      "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/streams/2014/March/140317/2D274905391507-today-tech-grandmabetty-140317.jpg",
+      "https://vetmed.tamu.edu/news/wp-content/uploads/sites/9/2021/11/Turtle-brumation-pet-talk-1024x767.jpeg",
+      "https://cdn.britannica.com/07/183407-050-C35648B5/Chicken.jpg"
+    ];
+
+    img.src = pics[whoNum];
+    document.getElementById("excuse").innerHTML = excuseArray;
+  }
+  btn.addEventListener("click", excuse);
 };
